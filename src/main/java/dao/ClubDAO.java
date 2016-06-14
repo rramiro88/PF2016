8dd87145f4986d1f783e1c65e40fcd65e69911c1
ramiro <ramiro@notebook>
2016-06-14 09:50:50 -0300

    algunas correcciones.

--- src/main/java/dao/ClubDAO.java
+++ src/main/java/dao/ClubDAO.java
@@ -7,7 +7,6 @@ package dao;
 
 import entidades.Club;
 import entidades.Estadio;
-import entidades.Jugador;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -21,32 +20,9 @@ public class ClubDAO {
         Club club = new Club();
         club.setNombre(nombreClub);
         club.setPresupuesto(0F);
-        club.setUrlEscudo("http://www.pubzi.com/f/md-Simple-shield.png");
+        club.setUrlEscudo("https://image.freepik.com/iconos-gratis/variante-escudo-con-bordes-blancos-y-negros_318-48076.png");
 
-//        Jugador jugador1 = new Jugador();
-//        jugador1.setNombre("Leonardo Pisculichi");
-//        club.agregarJugador(jugador1);
-//        
-//        Jugador jugador2 = new Jugador();
-//        jugador2.setNombre("Lucas Alario");
-//        club.agregarJugador(jugador2);
-//        
-//        Jugador jugador3 = new Jugador();
-//        jugador3.setNombre("Leonardo Ponzio");
-//        club.agregarJugador(jugador3);
-//        
-//        Jugador jugador4 = new Jugador();
-//        jugador4.setNombre("Marcelo Barovero");
-//        club.agregarJugador(jugador4);
-//        
-//        Jugador jugador5 = new Jugador();
-//        jugador5.setNombre("Jonathan Maidana");
-//        jugador4.setCotizacion(6000000.0);
-//        club.agregarJugador(jugador5);
-//        
-//        Jugador jugador6 = new Jugador();
-//        jugador6.setNombre("Rodrigo Mora");
-//        club.agregarJugador(jugador6);
+
         
         Estadio estadio = new Estadio();
         club.setEstadio(estadio);
@@ -58,12 +34,7 @@ public class ClubDAO {
         try {
 
             s.save(estadio);
-//            s.save(jugador1);
-//            s.save(jugador2);
-//            s.save(jugador3);
-//            s.save(jugador4);
-//            s.save(jugador5);
-//            s.save(jugador6);
+
             s.save(club);
             s.getTransaction().commit();
             s.close();
