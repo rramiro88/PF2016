d0cf7e857166339553ab4d6bc03d2185285382e6
ramiro <ramiro@notebook>
2016-08-14 12:07:48 -0300

    cambios en tactica2. Cambio en el dock.

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -1,56 +1,11 @@
 
- //sacado de http://jsfiddle.net/76yRN/1/
-                    jQuery(document).ready(function () {
+
+jQuery(document).ready(function () {
+
+//    $("#listaJugadores_data").sortable();
 
 
-
-                        jQuery.fn.swap = function (b) {
-                            // method from: http://blog.pengoworks.com/index.cfm/2008/9/24/A-quick-and-dirty-swap-method-for-jQuery
-                            b = jQuery(b)[0];
-                            var a = this[0];
-                            var t = a.parentNode.insertBefore(document.createTextNode(''), a);
-                            b.parentNode.insertBefore(a, b);
-                            t.parentNode.insertBefore(b, t);
-                            t.parentNode.removeChild(t);
-                            return this;
-                        };
-
-                        
-                        
-                        $(".dragdrop").draggable({zIndex:2500, appendTo: "body"});
-
-                        $(".dragdrop").droppable({
-                            accept: ".dragdrop",
-                            activeClass: "drag-activo",
-                            hoverClass: "drag-hover",
-                            drop: function (event, ui) {
-                                
-                                
-                                var draggable = ui.draggable,
-                                        droppable = $(this),
-                                        dragPos = draggable.position(),
-                                        dropPos = droppable.position();
-                                
-                                
-
-                                draggable.css({
-                                    left: dropPos.left + 'px',
-                                    top: dropPos.top + 'px',
-                                    zIndex: 999
-                                });
-
-                                droppable.css({
-                                    left: dragPos.left + 'px',
-                                    top: dragPos.top + 'px',
-                                    zIndex: 999
-                                });
-                                draggable.swap(droppable);
-                            }
-                        });
-
-
-
-                    });
+});
 
 
 /**
@@ -64,13 +19,13 @@
  * @returns {Boolean} no se utiliza el retorno
  */
 function verPosicionActual(objetoHTML) {
-    
+
     var c = objetoHTML.parentNode.childNodes;
-        
-    c[1].value=objetoHTML.style.left + "a" + objetoHTML.style.top;
-      
+
+    c[1].value = objetoHTML.style.left + "a" + objetoHTML.style.top;
+
     return false;
-    
+
 }
 
 /**
@@ -81,18 +36,95 @@ function verPosicionActual(objetoHTML) {
  * 
  */
 
-function cargarPosicionesActuales(){
-    
-    
+function cargarPosicionesActuales() {
+
+
     var listaDeJugadores = document.getElementById("listaJugadores");
-    
-       
-    for(i=1; i<22;i+=2){
-        listaDeJugadores.childNodes[i].childNodes[0].style.left=listaDeJugadores.childNodes[i].childNodes[1].value.split("a")[0];
-        listaDeJugadores.childNodes[i].childNodes[0].style.top=listaDeJugadores.childNodes[i].childNodes[1].value.split("a")[1];
+
+
+    for (i = 1; i < 22; i += 2) {
+        listaDeJugadores.childNodes[i].childNodes[0].style.left = listaDeJugadores.childNodes[i].childNodes[1].value.split("a")[0];
+        listaDeJugadores.childNodes[i].childNodes[0].style.top = listaDeJugadores.childNodes[i].childNodes[1].value.split("a")[1];
     }
-    
-    
+
+
 }
+
+//sacado de http://jsfiddle.net/76yRN/1/
+// $(".dragdrop").draggable({revert: "invalid"});
+//                    
+//                    
+//                    $(".dragdrop").droppable({
+//                        
+//                        activeClass: "dragActivo",
+//                        accept: ".dragdrop",
+//                        drop: function (event, ui){
+//                            
+//                            
+//                            
+//                            var copiaThis = $(this).clone();
+//                            var copiaDrop = ui.draggable.clone();
+//                            
+//                            $(this).replaceWith(copiaDrop);
+//                            ui.draggable.replaceWith(copiaThis);
+//                            
+//                            
+//                            
+//                            
+//                            
+//                            
+//                            
+//                            
+//                        }
+//                        
+//                        
+//                        
+//                    });
+//                    
+
+//                        jQuery.fn.swap = function (b) {
+//                            // method from: http://blog.pengoworks.com/index.cfm/2008/9/24/A-quick-and-dirty-swap-method-for-jQuery
+//                            b = jQuery(b)[0];
+//                            var a = this[0];
+//                            var t = a.parentNode.insertBefore(document.createTextNode(''), a);
+//                            b.parentNode.insertBefore(a, b);
+//                            t.parentNode.insertBefore(b, t);
+//                            t.parentNode.removeChild(t);
+//                            return this;
+//                        };
+//
+//                        
+//                        
+//                        $( ".dragdrop" ).draggable();
+//
+//                        $(".dragdrop").droppable({
+//                            accept: ".dragdrop",
+//                            activeClass: "drag-activo",
+//                            hoverClass: "drag-hover",
+//                            drop: function (event, ui) {
+//                                
+//                                
+//                                var draggable = ui.draggable,
+//                                        droppable = $(this),
+//                                        dragPos = draggable.position(),
+//                                        dropPos = droppable.position();
+//                                
+//                                
+//
+//                                draggable.css({
+//                                    left: dropPos.left + 'px',
+//                                    top: dropPos.top + 'px',
+//                                    zIndex: 999
+//
+//                                });
+//
+//                                droppable.css({
+//                                    left: dragPos.left + 'px',
+//                                    top: dragPos.top + 'px'
+//
+//                                });
+//                                draggable.swap(droppable);
+//                            }
+//                        });
 
 
