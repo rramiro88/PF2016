436d6a1f5f4625227b23bffef442c6fdda5d8a8b
ramiro <ramiro@notebook>
2016-10-29 10:24:53 -0300

    Cambio en estadisticas y grafico de escritorio. Completar.

--- src/main/java/controller/estadisticasController.java
+++ src/main/java/controller/estadisticasController.java
@@ -5,13 +5,16 @@
  */
 package controller;
 
+import entidades.TransaccionEconomica;
 import java.io.Serializable;
+import java.util.Date;
 import javax.annotation.PostConstruct;
 
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
-import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.DateAxis;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.LineChartSeries;
 
@@ -25,6 +28,9 @@ public class estadisticasController implements Serializable {
 
     private LineChartModel modeloAnimadoLineas;
 
+    @Inject
+    SesionController sesionController;
+
     @PostConstruct
     public void init() {
         createAnimatedModels();
@@ -35,35 +41,42 @@ public class estadisticasController implements Serializable {
         modeloAnimadoLineas.setTitle("Grafico economico/financiero");
         modeloAnimadoLineas.setAnimate(true);
         modeloAnimadoLineas.setLegendPosition("se");
-        Axis yAxis = modeloAnimadoLineas.getAxis(AxisType.Y);
-        yAxis.setMin(0);
-        yAxis.setMax(10);
 
     }
 
     private LineChartModel initLinearModel() {
         LineChartModel model = new LineChartModel();
 
+        model.getAxis(AxisType.X).setLabel("Dias");
+        model.getAxis(AxisType.Y).setLabel("$");
+
         LineChartSeries series1 = new LineChartSeries();
-        series1.setLabel("Ingresos por ventas");
+        series1.setLabel("Gastos / Ingresos");
 
-        series1.set(1, 2);
-        series1.set(2, 1);
-        series1.set(3, 3);
-        series1.set(4, 6);
-        series1.set(5, 8);
+        TransaccionEconomica t = null;
+        for (int i = 0; i < sesionController.getUsuarioLogueado().getClub().getTransacciones().size(); i++) {
 
-        LineChartSeries series2 = new LineChartSeries();
-        series2.setLabel("Egresos por compras");
+            t = sesionController.getUsuarioLogueado().getClub().getTransacciones().get(i);
 
-        series2.set(1, 6);
-        series2.set(2, 3);
-        series2.set(3, 2);
-        series2.set(4, 7);
-        series2.set(5, 9);
+            series1.set(t.getFecha().toString(), t.getMonto());
 
+        }
+
+//        LineChartSeries series2 = new LineChartSeries();
+//        series2.setLabel("Egresos por compras");
+//
+//        series2.set(1, 6);
+//        series2.set(2, 3);
+//        series2.set(3, 2);
+//        series2.set(4, 7);
+//        series2.set(5, 9);
         model.addSeries(series1);
-        model.addSeries(series2);
+//        model.addSeries(series2);
+
+        DateAxis axis = new DateAxis("Dates");
+        axis.setTickAngle(-50);
+
+        model.getAxes().put(AxisType.X, axis);
 
         return model;
     }
